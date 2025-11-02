95566dce3b37e465d2744972ff99dbe5b2ec66f7
ScytheGreg <grzegorzkaczmarek2017@gmail.com>
2025-11-02 12:17:28 +0100

    Restore original ocen.c file

--- Lab3/ocen.c
+++ Lab3/ocen.c
@@ -1,36 +1,34 @@
-#include "zbior_ary.h"
-#include <stdio.h>
+#include <stdbool.h>
 #include <assert.h>
-#include <limits.h>
-#include <stdlib.h>
-
-
+#include "zbior_ary.h"
 
 int main() {
-
-  zbior_ary A = ciag_arytmetyczny(-INT_MAX, INT_MAX , INT_MAX);
-
-  printf("Moc: %u\n", moc(A));
-
-  zbior_ary B = A;
-
-  for(int i = -INT_MAX + 5; i < INT_MAX  - 5; ++i){
-    ciag_ary a = {i, i};
-    ciag_ary* t_ciag = (ciag_ary*) malloc (sizeof(ciag_ary));
-    t_ciag[0] = a;
-    sumowalne_ciagi s = {
-      INT_MAX,
-      t_ciag,
-      1
-    };
-    przypisz_sumowalne(s, &B);
-    printf("Dodajemy: %d\n", i);
-  }
-
-  //wypisz_zbior(roznica(A, B));
-
-
-
-
+  // A = {2, 7, 12}
+  zbior_ary A = ciag_arytmetyczny(2, 5, 12);
+  assert(moc(A) == 3);
+  assert(ary(A) == 1);
+  // B = {4, 9, 14, 19}
+  zbior_ary B = ciag_arytmetyczny(4, 5, 19);
+  // C = {1}
+  zbior_ary C = singleton(1);
+  // D = {1, 4, 9, 14, 19}
+  zbior_ary D = suma(B, C);
+  // E = {1, 2, 4, 7, 9, 12, 14, 19}
+  zbior_ary E = suma(D, A);
+  assert(moc(E) == 8);
+  // F = {14}
+  zbior_ary F = singleton(14);
+  // G = {1, 2, 4, 7, 9, 12, 19}
+  zbior_ary G = roznica(E, F);
+  assert(ary(G) == 4);
+  assert(nalezy(G, 4));
+  assert(!nalezy(G, 5));
+  // H = {4, 9, 19};
+  zbior_ary H = iloczyn(G, B);
+  assert(moc(H) == 3);
+  assert(ary(H) == 2);
+  // I = {}
+  zbior_ary I = iloczyn(H, A);
+  assert(moc(I) == 0);
   return 0;
 }
