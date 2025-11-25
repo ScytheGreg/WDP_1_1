494d9ab1665379c28bf50a1307f281a93ea8cb5f
ScytheGreg <grzegorzkaczmarek2017@gmail.com>
2025-11-25 16:49:38 +0100

    Format, add comm. Lab7

--- Lab7/origami.c
+++ Lab7/origami.c
@@ -1,20 +1,20 @@
-//#include "debug.h"
+// Solution for "Origami" task. Grzegorz Kaczmarek
 #include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-const unsigned FIGURE_LIMIT = 1000;
+const unsigned FIGURE_LIMIT = 1e5;
 const unsigned LINE_LIMIT = 22;
-const double ZERO = 1e-6;
+const double ZERO = 1e-6; // Accepted calculation accuracy
 
 typedef struct {
   double x1, y1, x2, y2;
-} rect;
+} rect; // Rectangle
 
 typedef struct {
   double x, y, r;
-} circ;
+} circ; // Circle
 
 typedef enum { RECTANGLE, CIRCLE } shape_type;
 
@@ -34,162 +34,167 @@ typedef struct {
   };
   int l_cnt;
   line lines[];
-} shape;
+} shape; // Shape consists of a figure and batch of lines, along which figure
+         // was creased
 
 void input_error() {
   printf("Input error\n");
   exit(0);
 }
 
+// Allocates memory for new figure. If it is formed form existing sheet, copies
+// all elements
 shape *init_shape(shape_type fig_type, shape *prev_fig) {
-  // printf("Init_shape\n");
   shape *figure = (shape *)malloc(sizeof(shape) + LINE_LIMIT * sizeof(line));
   assert(figure != NULL);
   if (prev_fig != NULL)
     figure =
         memcpy(figure, prev_fig, sizeof(shape) + LINE_LIMIT * sizeof(line));
 
-  figure->type = fig_type;
-  // printf("Init_shape_end\n");
+  figure->type = fig_type; // Attribute type of figure.
   return figure;
 }
 
+// Reads a circle data, creates a sheet
 void read_circle(shape *figures[], int k) {
-  // printf("Read_circle\n");
   circ rc;
   if (scanf("%lf %lf %lf", &rc.x, &rc.y, &rc.r) != 3)
     input_error();
 
   figures[k] = init_shape(CIRCLE, NULL);
   figures[k]->circle = rc;
-  // printf("Read_circle_end %d\n", k);
 }
 
+// Reads a rectangle data, creates a sheet
 void read_rectangle(shape *figures[], int k) {
-  // printf("Read_rectangle\n");
   rect rr;
   if (scanf("%lf %lf %lf %lf", &rr.x1, &rr.y1, &rr.x2, &rr.y2) != 4)
     input_error();
 
   figures[k] = init_shape(RECTANGLE, NULL);
   figures[k]->rectangle = rr;
-  // printf("Read_rectangle_end %d\n", k);
 }
 
+// Reads crease data, creates copy of a sheet with new crease
 void read_line(shape *figures[], int k) {
   line rl;
   int idx;
   if (scanf("%d %lf %lf %lf %lf", &idx, &rl.x1, &rl.y1, &rl.x2, &rl.y2) != 5)
     input_error();
   assert(1 <= idx && idx < k);
-  shape *prev_fig = figures[idx];
-  figures[k] = init_shape(prev_fig->type, prev_fig);
+  shape *prev_fig = figures[idx];                    // Get dependent figure
+  figures[k] = init_shape(prev_fig->type, prev_fig); // Copy it
   int cnt = prev_fig->l_cnt;
-  figures[k]->lines[cnt] = rl;
+  figures[k]->lines[cnt] = rl; // Update lines
   figures[k]->l_cnt = cnt + 1;
-
-  // print_shape(figures[idx]);
 }
 
 typedef void (*read_object)(shape **, int);
 
+// Reads figures data
 void read_data(shape *figures[], int n) {
   for (int i = 1; i <= n; ++i) {
     char object_type;
     if (scanf("\n%c", &object_type) != 1)
       input_error();
-    assert((object_type == 'P' || object_type == 'K') || object_type == 'Z');
+    assert(object_type == 'P' || object_type == 'K' || object_type == 'Z');
 
-    read_object read_func[256] = {NULL};
+    read_object read_func[256] = {NULL}; // Table of functions for every char
     read_func['P'] = read_rectangle;
     read_func['K'] = read_circle;
     read_func['Z'] = read_line;
 
     if (read_func[(unsigned char)object_type] == NULL)
       input_error();
-    read_func[(unsigned char)object_type](figures, i);
+    read_func[(unsigned char)object_type](
+        figures, i); // For given char execute respective function.
   }
 }
+
+// Calculates distance form A to B
 double dist(point A, point B) {
   return (A.x - B.x) * (A.x - B.x) + (A.y - B.y) * (A.y - B.y);
 }
 
+// Checks whether the point is inside a figure
 int is_inside(shape *figure, double x, double y) {
-  //printf("Sprawdzamy punkt (%lf, %lf): ", x, y);
   if (figure->type == CIRCLE) {
     circ fc = figure->circle;
-   // printf("Czy jest: %d\n", dist((point){x, y}, (point){fc.x, fc.y}) <= fc.r * fc.r);
     return dist((point){x, y}, (point){fc.x, fc.y}) <= fc.r * fc.r + ZERO;
   }
   rect fr = figure->rectangle;
-  return (fr.x1 <= x + ZERO && x <= fr.x2 +ZERO && fr.y1 <= y + ZERO && y <= fr.y2 + ZERO);
+  return (fr.x1 <= x + ZERO && x <= fr.x2 + ZERO && fr.y1 <= y + ZERO &&
+          y <= fr.y2 + ZERO);
 }
 
+// Return reflected point to P by line L
 point reflect(point P, line L) {
-  //printf("Prosta: (%lf, %lf) , (%lf, %lf)\n", L.x1, L.y1, L.x2, L.y2);
   double dx = L.x2 - L.x1, dy = L.y2 - L.y1, a = dy, b = -dx,
          c = dx * L.y1 - dy * L.x1;
   double d = (a * P.x + b * P.y + c) / (a * a + b * b);
-  //printf("Odbicie punktu (%lf, %lf): (%lf, %lf)\n", P.x, P.y, P.x - 2 * a * d, P.y - 2 * b * d);
   return (point){P.x - 2 * a * d, P.y - 2 * b * d};
 }
 
+// Checks whether a point lies on the left (-1) or right (1) side of a line or
+// on the line (0)
 int proper_side(point P, line L) {
   double cross_product =
       (L.x2 - L.x1) * (P.y - L.y1) - (L.y2 - L.y1) * (P.x - L.x1);
 
-  //printf("Cross product: %lf\n", cross_product);
-  if (cross_product > ZERO){
-    //printf("Croos > 0\n");
-    return 1; // prawa strona
-  }
-  if (cross_product < -ZERO){
-    //printf("Cross < 0\n");
-    return -1; // lewa strona
-  }
-  //printf("Na prostej\n");
-  return 0;    // na prostej
+  if (cross_product > ZERO)
+    return 1; // Right side
+
+  if (cross_product < -ZERO)
+    return -1; // Left side
+
+  return 0; // On the line
 }
 
+// Recursion function. Returns number of layers under given point after l_ix
+// crease
 int rec(shape *figure, double x, double y, int l_ix) {
-  if (l_ix < 0)
+  if (l_ix < 0) // If no more lines, just return whether it belongs
     return (is_inside(figure, x, y));
 
   line our_line = figure->lines[l_ix];
   point p = reflect((point){x, y}, our_line);
-  if (proper_side((point){x, y}, our_line) == 1)
+  if (proper_side((point){x, y}, our_line) ==
+      1) // Trigger function from this point and it's reflection
     return rec(figure, x, y, l_ix - 1) + rec(figure, p.x, p.y, l_ix - 1);
 
-  else if (proper_side((point){x, y}, our_line) == 0){
+  else if (proper_side((point){x, y}, our_line) ==
+           0) { // If lies on the line trigger just from this point
     return (rec(figure, x, y, l_ix - 1));
   }
 
-  return 0; // if proper_side(x, y, our_line) == -1
+  return 0; // If lies on bad side of the line, just return 0
 }
 
+// Main function pinning whole solution
 void solve() {
   int n, q;
   if (scanf("%d %d", &n, &q) != 2)
     input_error();
 
-  shape **figures = (shape **)malloc((unsigned)(n + 1) * sizeof(shape *));
+  shape **figures = (shape **)malloc((unsigned)(n + 1) *
+                                     sizeof(shape *)); // Main table of figures
   assert(figures != NULL);
-  for (int i = 0; i <= n; i++)
+  for (int i = 0; i <= n; i++) // Set every pointer to null
     figures[i] = NULL;
 
   read_data(figures, n);
 
-  for (int i = 0; i < q; ++i) {
+  for (int i = 0; i < q; ++i) { // Answering enquirers
     int k;
     double x, y;
     if (scanf("%d %lf %lf", &k, &x, &y) != 3)
       input_error();
 
-    // printf("K: %d\n", k);
     assert(figures[k] != NULL);
     printf("%d\n", rec(figures[k], x, y, figures[k]->l_cnt - 1));
   }
-  for (int i = 0; i <= n; i++)
+
+  for (int i = 0; i <= n; i++) // Clean works
     free(figures[i]);
   free(figures);
 }
