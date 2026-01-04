3caec6fe232e2a0cd5ae95edf5b2a1a85ad10dd6
ScytheGreg <grzegorzkaczmarek2017@gmail.com>
2026-01-04 11:49:08 +0100

    Debug. Lab11

--- Lab11/prz.cpp
+++ Lab11/prz.cpp
@@ -1,6 +1,4 @@
-#include <iostream>
-#include <vector>
-#include <set>
+#include <bits/stdc++.h>
 #define ll long long
 
 using namespace std;
@@ -13,24 +11,46 @@ using vec = vector<point>;
 
 struct ivl{
     int beg, end;
+    double quality;
+    ivl(int b, int e, const vec& seq){
+        beg = b; end = e;
+        quality = ((double) (seq[e].x - seq[b].x)) / sqrt(e - b + 1);
+    }
+    bool operator<(const ivl& other) const {
+        constexpr double eps = 1e-6;
+
+        if (quality < other.quality - eps) return true;
+        if (quality > other.quality + eps) return false;
+
+        return beg > other.beg;
+    }
 };
+
+void print_set(const std::multiset<ll>& s) {
+    for (ll v : s)
+        std::cout << v << ' ';
+    std::cout << '\n';
+}
+
 struct params{
     int n;
     ll U;
     vec seq;
-}
+};
 
-void read_data(params& par){
-    cin >> par.n >> par.U;
-    par.seq.push_back({0,0});
-    for(int i = 0 ; i < n ; i++){
+void read_data(params& p){
+    cin >> p.n >> p.U;
+    p.seq.push_back({0,0});
+    for(int i = 0 ; i < p.n ; i++){
         ll x, y;
         cin >> x >> y;
-        par.seq.push_back({x, y});
+        p.seq.push_back({x, y});
     }
 }
 
 bool is_U_strict(multiset<ll>& set, const ll U, ll y){
+    if(set.size() == 0)
+        return true;
     ll Min = *set.begin();
     ll Max = *set.rbegin();
     assert(Min <= Max);
@@ -39,40 +59,50 @@ bool is_U_strict(multiset<ll>& set, const ll U, ll y){
     return true;
 }
 
-maximize_ivl(const params& p, int& k, multiset>ll>& set){
-    while(k < p.n - 1 && is_U_strict(set, p.U, p.seq[k + 1].y)){
+void maximize_ivl(const params& p, int& k, multiset<ll>& set){
+    while(k < p.n  && is_U_strict(set, p.U, p.seq[k + 1].y)){
         set.insert(p.seq[k + 1].y);
         k++;
     }
 }
 
-void add_result(ivl cand, ivl& prev, vector<ivl>& res){
+void set_del(ll y, multiset<ll>& set){
+    auto it = set.find(y);
+    assert(it != set.end());
+    set.erase(it);
+}
+
+void add_ivl(ivl cand, ivl& prev, priority_queue<ivl>& res){
     assert(cand.beg <= cand.end);
     if(cand.end == prev.end)
-        res.push_back(prev);
+        res.push(prev);
     else{
-        res.push_back(cand);
+        res.push(cand);
         prev = cand;
     }
 }
 
-void set_del(ll y){
-    auto it = set.find(y);
-    set.erase(it);
+void print_res(int i, priority_queue<ivl>& res){
+    while(res.top().end < i){
+        res.pop();
+        assert(!res.empty());
+    }
+    cout << res.top().beg << ' ' << res.top().end << '\n';
 }
 
-vector<ivl> dp(const params& p){
-    vector<ivl> res(1);
+
+void dp(const params& p){
+    priority_queue<ivl> res;
     int k = 1;
-    ivl prev = -1;
+    ivl prev = ivl(0, 0, p.seq);
     multiset<ll> set;
+    set.insert(p.seq[1].y);
     for(int i = 1 ; i <= p.n ; ++i){
-        set.insert(p.seq[i].y);
         maximize_ivl(p, k, set);
-        add_result({i, k}, prev, res);
-        set_del(p.seq[i].y);
+        set_del(p.seq[i].y, set);
+        add_ivl(ivl(i, k, p.seq), prev, res);
+        print_res(i, res);
     }
-    return res;
 }
 
 void solve(){
